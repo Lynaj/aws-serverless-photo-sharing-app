aa0931785f5e841efa360f10a732ff6b3ee0ca2d
Artur Drozdzyk <arturdrozdzyk@gmail.com>
2020-05-03 13:47:52 +0400

    Inital commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Header } from 'semantic-ui-react';
+
+function App() {
+  return (
+    <Header as="h1">
+      Hello World!
+    </Header>
+  )
+}
+
+export default App
